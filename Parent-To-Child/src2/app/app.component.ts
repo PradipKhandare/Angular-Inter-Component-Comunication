6a2ca9e00b0dff7791d19bef43adce3d3d221c80
Pradip Khandare <121931206+PradipKhandare@users.noreply.github.com>
2023-04-19 11:56:05 +0530

    Add files via upload

---
+++ Parent-To-Child/src2/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Structural-Directive';
+
+  public message = "Hello child component, this is your parent!!"
+}
